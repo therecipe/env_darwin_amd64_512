1e4e52080fad7db9b99cf26e6fc757959f2b4d24
therecipe <therecipe@users.noreply.github.com>
2018-12-22 19:38:50 +0100

    uniform init fix

--- init.sh
+++ init.sh
@@ -6,11 +6,14 @@ QT_VERSION=5.12.0
 rm -rf ./${QT_VERSION}
 rm -rf ./Licenses
 
+
 rsync -avz $QT_ROOT/Qt/${QT_VERSION}/clang_64 ./${QT_VERSION}/
 rsync -avz $QT_ROOT/Qt/Licenses .
 
+
 rm -rf ./${QT_VERSION}/clang_64/{doc,phrasebooks}
-rm -rf ./${QT_VERSION}/clang_64/lib/{cmake,pkgconfig}
+rm -rf ./${QT_VERSION}/clang_64/lib/{cmake,pkgconfig,libQt5Bootstrap.a}
+
 
 for v in *.jsc *.log *.pro *.pro.user *.qmake.stash *.qmlc .DS_Store *_debug* *.dSYM *.la *.prl; do
 	find . -name ${v} -exec rm -rf {} \;
@@ -18,7 +21,7 @@ done
 
 mkdir -p ./${QT_VERSION}/clang_64/_bin
 for v in macdeployqt moc qmake qmlimportscanner qt.conf rcc uic; do
-	mv ./${QT_VERSION}/clang_64/bin/${v} ./${QT_VERSION}/clang_64/_bin/${v}
+	mv ./${QT_VERSION}/clang_64/bin/${v} ./${QT_VERSION}/clang_64/_bin/
 done
 rm -rf ./${QT_VERSION}/clang_64/bin && mv ./${QT_VERSION}/clang_64/_bin ./${QT_VERSION}/clang_64/bin
 
